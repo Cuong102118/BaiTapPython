c36f26ab443ae34184ad8a8a1b421ab0b5441243
Cuong102118 <cuong102118@st.vimaru.edu.vn>
2024-12-10 08:28:40 +0700

    Update Bai 3.py

--- Chuong 2/Bai 3.py
+++ Chuong 2/Bai 3.py
@@ -1,23 +1,18 @@
 import math
 
 def PTBacHai(a, b, c):
-    if a == 0:
-        return "Hệ số a phải khác 0."
-    
     delta = b**2 - 4*a*c
     
     if delta > 0:
         x1 = (-b + math.sqrt(delta)) / (2 * a)
         x2 = (-b - math.sqrt(delta)) / (2 * a)
-        return "Phương trình có hai nghiệm phân biệt: x1 = {0}, x2 = {1}".format(x1,x2)
+        return "{0} {1}".format(x1,x2)
     elif delta == 0:
         x = -b / (2 * a)
-        return "Phương trình có nghiệm kép: x = {0}".format(x)
+        return "{0}".format(x)
     else:
-        return "Phương trình vô nghiệm."
+        return "VN"
 
-a = float(input("Nhập a: "))
-b = float(input("Nhập b: "))
-c = float(input("Nhập c: "))
+a, b, c = map(float,input().split())
 kq = PTBacHai(a, b, c)
 print(kq)
