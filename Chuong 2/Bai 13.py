82e952daaa0535f8049c7627fee0a6a7f8efed69
Cuong102118 <cuong102118@st.vimaru.edu.vn>
2024-12-10 08:23:55 +0700

    Update Bai 13.py

---
+++ Chuong 2/Bai 13.py
@@ -0,0 +1,15 @@
+import math
+
+def TinhTong(n):
+    S = 0
+    for i in range(n + 1):
+        SoLe = 2 * i + 1
+        S += (math.factorial(SoLe))**(-1)
+    return S
+
+n = int(input())
+if n >= 0:
+    kq = TinhTong(n)
+    print("{kq}".format(kq))
+else:
+    print("n phải là số nguyên không âm.")
